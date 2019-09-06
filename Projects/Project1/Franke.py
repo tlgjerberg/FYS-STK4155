e667fc26f97ba901031cead69609a0aec610375a
Torbjørn <torgje@lavabit.com>
2019-09-06 15:37:45 +0200

    Separated code leaving functions in this file. Added MSE and R-squared function. Calculated zpredict

--- Projects/Project1/Franke.py
+++ Projects/Project1/Franke.py
@@ -1,20 +1,28 @@
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
+from numpy import linalg
 from random import random, seed
+from designMatrix import CreateDesignMatrix_X
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-# Make data.
-x = np.arange(0, 1, 0.05)
-y = np.arange(0, 1, 0.05)
-eps = 0.1 * np.random.normal(0, 1, 400).reshape(np.size(x),
-                                                np.size(y))  # Noise matrix
-x, y = np.meshgrid(x, y)
 
-print(eps)
+def CreateDesignMatrix_X(x, y, n=5):
+    """
+    Function for creating a design X-matrix with rows [1, x, y, x^2, xy, xy^2 , etc.]
+    Input is x and y mesh or raveled mesh, keyword agruments n is the degree of the polynomial you want to fit.
+    """
+    if len(x.shape) > 1:
+        x = np.ravel(x)
+        y = np.ravel(y)
+
+    N = len(x)
+    l = int((n + 1) * (n + 2) / 2)		# Number of elements in beta
+    X = np.ones((N, l))
+
+    for i in range(1, n + 1):
+        q = int((i) * (i + 1) / 2)
+        for k in range(i + 1):
+            X[:, q + k] = x**(i - k) * y**k
+
+    return X
 
 
 def FrankeFunction(x, y):
@@ -25,14 +33,10 @@ def FrankeFunction(x, y):
     return term1 + term2 + term3 + term4 + eps  # Added noise term eps
 
 
-z = FrankeFunction(x, y)
-# Plot the surface.
-surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-# Customize the z axis.
-ax.set_zlim(-0.10, 1.40)
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('% .02f'))
-# Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
-plt.show()
+def MSE(y_data, y_predict):
+    n = np.size(y_predict)
+    return np.sum((y_data - y_predict)**2) / n
+
+
+def R2(y_data, y_predict):
+    return 1 - np.sum((y_data - y_predict)**2) / np.sum((y_data - np.mean(y_data))**2)
