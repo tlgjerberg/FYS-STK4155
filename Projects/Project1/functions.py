edecebaf2729089ece7bf4b037dba8e9ede2b1ea
Torbjørn <torgje@lavabit.com>
2019-09-18 14:42:38 +0200

    Changed KFoldCrossValidation to return the MSE of training and test data instead of only test data

--- Projects/Project1/functions.py
+++ Projects/Project1/functions.py
@@ -66,66 +66,76 @@ def KFoldCrossValidation(x, y, z, k, p):
     # print(x)
 
     # KFold instance
-    kfold = KFold(n_splits=k)
+    kfold = KFold(n_splits=k, shuffle=True)
 
-    MSE = np.zeros(k)
-    R2 = np.zeros(k)
+    MSE_test = np.zeros(k)
+    MSE_train = np.zeros(k)
+    R2_test = np.zeros(k)
+    R2_train = np.zeros(k)
     beta = np.zeros((k, int((p + 1) * (p + 2) / 2)))
-    tot_R2_estimate = tot_MSE_estimate = 0
+    tot_R2_estimate_test = tot_MSE_estimate_test = 0
+    tot_R2_estimate_train = tot_MSE_estimate_train = 0
     index = 0
     jndex = 0
 
     for train_ind, test_ind in kfold.split(x):
 
         # Assigning train and test data
-        x_train_cv = x[train_ind]
-        x_test_cv = x[test_ind]
+        x_train = x[train_ind]
+        x_test = x[test_ind]
 
-        y_train_cv = y[train_ind]
-        y_test_cv = y[test_ind]
+        y_train = y[train_ind]
+        y_test = y[test_ind]
 
-        z_train_cv = z[train_ind]
-        z_test_cv = z[test_ind]
+        z_train = z[train_ind]
+        z_test = z[test_ind]
 
         # Raveling z data into 1D arrays
-        z_train_cv_1d = np.ravel(z_train_cv)
-        z_test_cv_1d = np.ravel(z_test_cv)
+        z_train_1d = np.ravel(z_train)
+        z_test_1d = np.ravel(z_test)
 
         # OLS to find a model for prediction using train data
 
         # Setting up the design matrices for training and test data
-        XY_train_cv = CreateDesignMatrix_X(x_train_cv, y_train_cv, n=p)
-        XY_test_cv = CreateDesignMatrix_X(x_test_cv, y_test_cv, n=p)
+        XY_train = CreateDesignMatrix_X(x_train, y_train, n=p)
+        XY_test = CreateDesignMatrix_X(x_test, y_test, n=p)
 
         # Inverting
-        # XY2_cv_inv = np.linalg.inv(XY_train_cv.T.dot(XY_train_cv))
-        XY2_cv_inv_SVD = MatrixInvSVD(XY_train_cv.T.dot(XY_train_cv))
+        # XY2_inv = np.linalg.inv(XY_train.T.dot(XY_train))
+        XY2_inv_SVD = MatrixInvSVD(XY_train.T.dot(XY_train))
 
-        # XY2_cv_inv = MatrixInvSVD(XY_train_cv)
+        # XY2_inv = MatrixInvSVD(XY_train)
 
         # Model parameters beta based on training data design matrix
-        beta_cv = XY2_cv_inv_SVD.dot(XY_train_cv.T).dot(z_train_cv_1d)
+        beta = XY2_inv_SVD.dot(XY_train.T).dot(z_train_1d)
 
         # Combining test design matrix with model parameters
-        z_testPred_cv = XY_test_cv @ beta_cv
+        z_testPred = XY_test @ beta
+        z_trainPred = XY_train @ beta
 
-        MSE[index] = mean_squared_error(z_test_cv_1d, z_testPred_cv)
-        R2[index] = r2_score(z_test_cv_1d, z_testPred_cv)
-        # beta[index, :] = beta_cv
+        MSE_test[index] = mean_squared_error(z_test_1d, z_testPred)
+        R2_test[index] = r2_score(z_test_1d, z_testPred)
+        MSE_train[index] = mean_squared_error(z_train_1d, z_trainPred)
+        R2_train[index] = r2_score(z_train_1d, z_trainPred)
+        # beta[index, :] = beta
         # print(R2[index])
 
-        tot_MSE_estimate += MSE[index]
-        tot_R2_estimate += R2[index]
+        tot_MSE_estimate_test += MSE_test[index]
+        tot_R2_estimate_test += R2_test[index]
+        tot_MSE_estimate_train += MSE_train[index]
+        tot_R2_estimate_train += R2_train[index]
         # print(tot_MSE_estimate)
 
         index += 1
 
     # print(MSE)
     # print(tot_MSE_estimate)
-    tot_MSE_estimate /= k
-    tot_R2_estimate /= k
+    tot_MSE_estimate_test /= k
+    tot_R2_estimate_test /= k
+    tot_MSE_estimate_train /= k
+    tot_MSE_estimate_train /= k
     # print(tot_MSE_estimate)
     # print(tot_R2_estimate)
     # print(np.argmax(R2))
 
-    return tot_MSE_estimate, tot_R2_estimate
+    return tot_MSE_estimate_test, tot_MSE_estimate_train
