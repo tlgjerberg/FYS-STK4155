885c55f74d8639d42c3b756c8d014043cedffaa1
Torbjørn <torgje@lavabit.com>
2019-11-01 17:13:17 +0100

    Commit before attempting to change feedforward

--- Projects/Project2/NeuralNetNew.py
+++ Projects/Project2/NeuralNetNew.py
@@ -2,23 +2,26 @@ import numpy as np
 
 
 class NeuralNetwork:
-    def __init__(self, layer_sizes, seed=0):
+    def __init__(self, layer_sizes, num_param, num_inputs, seed=0):
         self.layer_sizes = layer_sizes
         self.num_layers = len(layer_sizes)
-        self.weights = np.empty(self.num_layers, dtype=np.ndarray)
-        self.biases = np.empty(self.num_layers, dtype=np.ndarray)
+        self.num_param = num_param
+        self.num_inputs = num_inputs
+        self.act = np.empty(self.num_layers + 2, dtype=np.ndarray)
+        self.weights = np.empty(self.num_layers + 1, dtype=np.ndarray)
+        self.biases = np.empty(self.num_layers + 1, dtype=np.ndarray)
 
         np.random.seed(seed)
         self._architecture()
 
     def _architecture(self):
-        # self.weights[0] = np.random.randn(self.X.shape[1], self.layer_sizes[0])
-        # self.biases[0] = np.zeros(sizes[0]) + 0.01
+        self.weights[0] = np.random.randn(self.num_param, self.layer_sizes[0])
+        self.biases[0] = np.random.randn(1, self.num_inputs)
 
         for l in range(1, self.num_layers):
             self.weights[l] = np.random.randn(
                 self.layer_sizes[l - 1], self.layer_sizes[l])
-            self.biases = np.random.randn(1, self.layer_sizes[l])
+            self.biases = np.random.randn(self.layer_sizes[l])
 
     def _sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
@@ -27,7 +30,7 @@ class NeuralNetwork:
         exps = np.exp(z)
         return exps / np.sum(exps)
 
-    def activation_function():
+    def activation_function(self):
         pass
 
     def FeedForward(self):
@@ -37,10 +40,7 @@ class NeuralNetwork:
         self.targets = []
 
         for b, w in zip(self.biases, self.weights):
-            # print("weights", w.T.shape)
-            # print("X", self.X.shape)
-            # print("biases", b.shape)
-            z = np.matmul(self.X, w) + b
+            z = np.matmul(self.X, w)
             print(z.shape)
             a = self._sigmoid(z)
             self.zList.append(z)
@@ -80,7 +80,7 @@ class NeuralNetwork:
     def MBSDG(self, X, y, n_iters=100, eta=1e-4, epochs=10, batch_size=10):
         self.X_full = X
         self.y_full = y
-        print("full x", self.X_full.shape)
+        # print("full x", self.X_full.shape)
 
         data_indices = np.arange(len(self.y_full))
 
--- Projects/Project2/testnn.py
+++ Projects/Project2/testnn.py
@@ -4,15 +4,15 @@ from sklearn.datasets import load_breast_cancer
 
 X, y = load_breast_cancer(return_X_y=True)
 
-# print(X.shape)
+# print(X.shape[1])
 # print(y.shape)
 
-NN = NeuralNetwork([y.size, 10, 5])
+NN = NeuralNetwork([y.size, 10, 5], X.shape[1], X.shape[0])
 
 
-print("biases", NN.biases)
-print("weights", NN.weights)
-print(NN.layer_sizes)
+# print("biases", NN.biases.shape)
+# print("weights", NN.weights.shape)
+# print(NN.layer_sizes)
 
 
 NN.MBSDG(X, y)
