Fixed an issue with data assignment and backpropagation

Projects/Project2/NeuralNetNew.py
@@ -5,8 +5,8 @@ class NeuralNetwork:
     def __init__(self, layer_sizes, X, y, seed=0):
         self.layer_sizes = layer_sizes
         self.num_layers = len(layer_sizes) - 1
-        self.X = X
-        self.y = y
+        self.X_data = X
+        self.y_data = y
         self.act = np.empty(self.num_layers + 2, dtype=np.ndarray)
         self.weights = np.empty(self.num_layers + 1, dtype=np.ndarray)
         self.biases = np.empty(self.num_layers + 1, dtype=np.ndarray)
@@ -15,8 +15,9 @@ class NeuralNetwork:
         self._architecture()
 
     def _architecture(self):
-        self.weights[0] = np.random.randn(self.X.shape[1], self.layer_sizes[0])
-        self.biases[0] = np.random.randn(self.X.shape[1])
+        self.weights[0] = np.random.randn(
+            self.X_data.shape[1], self.layer_sizes[0])
+        self.biases[0] = np.random.randn(self.X_data.shape[1])
 
         for l in range(1, self.num_layers + 1):
             self.weights[l] = np.random.randn(
@@ -41,8 +42,8 @@ class NeuralNetwork:
         self.targets = []
 
         for b, w in zip(self.biases, self.weights):
-            print("act", activation.shape)
-            print("w", w.shape)
+            # print("act", activation.shape)
+            # print("w", w.shape)
             z = np.matmul(activation, w) + b
             activation = self._sigmoid(z)
             self.zList.append(z)
@@ -66,14 +67,18 @@ class NeuralNetwork:
         grad_b_out = np.sum(delta_out, axis=0)
         grad_w_out = np.matmul(self.act[-1].T, delta_out)
 
+        # Hidden layers
+        grad_w_hidden = np.empty(self.num_layers)
+        grad_b_hidden = np.empty(self.num_layers)
+
         for l in range(2, self.num_layers):
             z = zList[-l]
             delta_hidden = (np.matmul(
                 self.weights[-l + 1].T, delta_out)
                 * self.act[-l] * (1 - self.act[-l]))
 
-            grad_b_hidden = np.sum(delta_hidden, axis=0)
-            grad_w_hidden = np.matmul(delta_hidden, self.act[-l - 1].T)
+            grad_b_hidden[-l] = np.sum(delta_hidden, axis=0)
+            grad_w_hidden[-l] = np.matmul(delta_hidden, self.act[-l - 1].T)
 
     def getMiniBatches(self, data_indices, batch_size):
 
@@ -83,17 +88,17 @@ class NeuralNetwork:
         return random_indcs
 
     def MBSDG(self, n_iters=100, eta=1e-4, epochs=10, batch_size=10):
-        # print("full x", self.X_full.shape)
 
-        data_indices = np.arange(len(self.y_full))
+        data_indices = np.arange(len(self.y_data))
+
+        print(self.X_data.shape)
 
         for i in range(epochs):
             for j in range(n_iters):
 
                 random_indcs = self.getMiniBatches(data_indices, batch_size)
-
-                self.X = self.X_full[random_indcs]
-                self.y = self.y_full[random_indcs]
+                self.X = self.X_data[random_indcs]
+                self.y = self.y_data[random_indcs]
 
                 self.BackPropagation()
 